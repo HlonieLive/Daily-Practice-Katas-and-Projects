1a869a615cd7b68525e1039cb888fa3515e65d5b
Lehlohonolo Tshabalala <sucsido@gmail.com>
2025-02-23 23:57:39 +0200

    Implement longest_common_prefix function

--- Python/Katas/kata_08/mock_lists.py
+++ Python/Katas/kata_08/mock_lists.py
@@ -79,7 +79,13 @@ Input: ["flower", "flow", "flight"]
 Output: 'fl'"""
 def longest_common_prefix(strs):
     # Your implementation here
-    pass
+    if not strs:
+        return ""
+    prefix = strs[0]
+    for string in strs[1:]:
+        while string[:len(prefix)] != prefix and prefix:
+            prefix = prefix[:-1]
+    return prefix
 
 """TODO: Question 8 - Control Structures
 Write a function `count_primes(n)` that counts the number of prime numbers less than
