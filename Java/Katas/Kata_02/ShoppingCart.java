5a2e744d93b0afb8cef757ceecb176053beb6b22
Lehlohonolo Tshabalala <letshabjhb024@student.wethinkcode.co.za>
2025-04-09 11:08:34 +0200

    Updated ShoppingCart

--- Java/Katas/Kata_02/ShoppingCart.java
+++ Java/Katas/Kata_02/ShoppingCart.java
@@ -1,28 +1,53 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+class Item {
+    String name;
+    double price;
+    int quantity;
+
+    public Item(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    public double getTotal() {
+        return price * quantity;
+    }
+}
 
 public class ShoppingCart {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-        String item;
-        double price;
-        int quality;
+        ArrayList<Item> cart = new ArrayList<>();
         char currency = 'R';
-        double total;
 
-        System.out.print("Which item would you like to buy? ");
-        item = scanner.nextLine();
+        while (true){
+            System.out.print("Enter item name (or type 'done' to checkout): ");
+            String itemName = scanner.nextLine();
 
-        System.out.print("What is the price? ");
-        price = scanner.nextDouble();
+            if (itemName.equals("done")) break;
 
-        System.out.print("How many would you like to buy?");
-        quality = scanner.nextInt();
+            System.out.print("Enter price: ");
+            double itemPrice = scanner.nextDouble();
 
-        total = price * quality;
+            System.out.print("Enter quantity: ");
+            int itemQuantity = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.print("Your Total is: " + currency + total + "\nCheckout...");
+            cart.add(new Item(itemName, itemPrice, itemQuantity));
+        }
+
+        double grandTotal = 0;
+        System.out.println("Receipt:");
+        for (Item item: cart) {
+            System.out.printf("%s x%d @ %c%.2f each = %c%.2f%n", item.name, item.quantity, currency, item.price, currency, item.getTotal());
+            grandTotal += item.getTotal();
+        }
+        System.out.printf("Total: %c%.2f%n", currency, grandTotal);
+        System.out.println("\nCheckout complete. Thank you!");
 
         scanner.close();
     }
