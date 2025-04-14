449ecf513ecf0f8ddc4292911a0a019f85bc7c06
Lehlohonolo Tshabalala <sucsido@gmail.com>
2025-04-14 23:13:43 +0200

    Refactoring Weight Comverter class

--- Java/Katas/Kata_04/WeightConverter.java
+++ Java/Katas/Kata_04/WeightConverter.java
@@ -12,23 +12,25 @@ public class WeightConverter {
         System.out.println("1. Convert lbs to kgs.");
         System.out.println("2. Convert kgs to lbs.");
 
+        // Prompt for user choice
         System.out.println("Choose your option: ");
         choice = scanner.nextInt();
+
+        // Option 1: convert lbs to kgs
+        // Option 2: convert kgs to lbs
+        // Invalid choice
+
 
         if (choice == 1) {
             System.out.println("Enter the weight in lbs: ");
             weight = scanner.nextDouble();
             newWeight = weight * 0.453592;
             System.out.println("The new weight in kgs is: "+ newWeight);
-        }
-
-
-        // Prompt for user choice
-
-        // Option 1: convert lbs to kgs
-
-        // Option 2: convert kgs to lbs
-
-        // Invalid choice
+        }else if (choice == 2) {
+            System.out.println("Enter the weight in kgs: ");
+            weight = scanner.nextDouble();
+            newWeight = weight / 0.453592;
+            System.out.println("The new weight in lbs is: "+ newWeight);
+        } else System.out.println("Invalid Choice");
     }
 }
