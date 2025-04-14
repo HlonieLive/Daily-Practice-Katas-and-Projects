341fc91a4778626987d64a3e9cac8d046befbb56
Lehlohonolo Tshabalala <sucsido@gmail.com>
2025-04-14 22:55:41 +0200

    Adding Weight Comverter class pseudocode

---
+++ Java/Katas/Kata_04/WeightConverter.java
@@ -0,0 +1,13 @@
+public class WeightConverter {
+    public static void main(String[] args) {
+        // Weight Coversion Program
+
+        // Prompt for user choice
+
+        // Option 1: convert lbs to kgs
+
+        // Option 2: convert kgs to lbs
+
+        // Invalid choice
+    }
+}
